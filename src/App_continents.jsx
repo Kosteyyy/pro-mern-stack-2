ca2d69765200fbffb323230d0c6c9921b0b7b572
Kosteyyy <averkiev@yandex.ru>
2021-01-08 02:07:03 +0300

    first version of IssueTracker

---
+++ src/App_continents.jsx
@@ -0,0 +1,14 @@
+const continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
+const helloContinents = Array.from(continents, c => `Hello ${c}!`);
+const message = helloContinents.join(' ');
+
+
+const element = (
+			<div title="Outer div">
+				<h1 className="myHeader">New {message}</h1>
+			</div>
+		);
+
+ReactDOM.render(element, document.getElementById('content'));
+
+
