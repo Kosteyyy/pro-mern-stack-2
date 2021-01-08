340cd5e836e1aebf5a30f9b35c35e258d639e3be
Kosteyyy <averkiev@yandex.ru>
2021-01-08 23:10:11 +0300

    MongoDB read-write

--- server/server.js
+++ server/server.js
@@ -3,19 +3,28 @@ const express = require('express');
 const { ApolloServer, UserInputError } = require('apollo-server-express');
 const { GraphQLScalarType } = require('graphql');
 const { Kind } = require('graphql/language');
+const { MongoClient } = require('mongodb');
+
+const url = 'mongodb://localhost/issuetracker';
+// Atlas URL - replace UUU with user, PPP with password, XXX with hostname
+// const url = 'mongodb+srv://UUU:PPP@cluster0-XXX.mongodb.net/issuetracker?retryWrites=true';
+// mLab URL - replace UUU with user, PPP with password, XXX with hostname
+// const url = 'mongodb://UUU:PPP@XXX.mlab.com:33533/issuetracker';
+
+let db;
 
 let aboutMessage = "Issue Tracker API v1.0";
 
-const issuesDB = [
-	{
-		id: 1, status: 'New', owner: 'Ravan', effort: 5, created: new Date('2018-08-15'), 
-		due: undefined, title: "Error in console when clicking Add",
-	},
-	{
-		id: 2, status: 'Assigned', owner: 'Eddie', effort: 14, created: new Date('2018-08-16'),
-		due: new Date('2018-08-30'), title: 'Missing bottom border on panel',
-	},
-];
+// const issuesDB = [
+// 	{
+// 		id: 1, status: 'New', owner: 'Ravan', effort: 5, created: new Date('2018-08-15'), 
+// 		due: undefined, title: "Error in console when clicking Add",
+// 	},
+// 	{
+// 		id: 2, status: 'Assigned', owner: 'Eddie', effort: 14, created: new Date('2018-08-16'),
+// 		due: new Date('2018-08-30'), title: 'Missing bottom border on panel',
+// 	},
+// ];
 
 const GraphQLDate = new GraphQLScalarType({
 	name: 'GraphQLDate',
@@ -60,21 +69,40 @@ function issueValidate(issue) {
 	}
 }
 
-function issueAdd(_, { issue }) {
+async function getNextSequence(name) {
+	const result = await db.collection('counters').findOneAndUpdate(
+		{ _id: name },
+		{ $inc: { current: 1 }},
+		{ returnOriginal: false },
+	);
+	return result.value.current;
+}
+
+
+async function issueAdd(_, { issue }) {
 	issueValidate(issue);
 	issue.created = new Date();
-	issue.id = issuesDB.length + 1;
-	if (issue.status == undefined) issue.status = 'New';
-	issuesDB.push(issue);
-	return issue;
+	issue.id = await getNextSequence('issues');
+	const result = await db.collection('issues').insertOne(issue);
+	const savedIssue = await db.collection('issues')
+		.findOne({ _id: result.insertedId });
+	return savedIssue;
 }
 
 function setAboutMessage(_, { message }) {
 	return aboutMessage = message;
 }
 
-function issueList() {
-	return issuesDB;
+async function issueList() {
+	const issues = await db.collection('issues').find({}).toArray();
+	return issues;
+}
+
+async function connectToDb() {
+	const client = new MongoClient(url, { useUnifiedTopology: true, useNewUrlParser: true });
+	await client.connect();
+	console.log('Connected to MongoDB at ', url);
+	db = client.db();
 }
 
 const server = new ApolloServer({
@@ -91,7 +119,13 @@ const app = express();
 app.use(express.static('public'));
 
 server.applyMiddleware({ app, path: '/graphql' });
-
-app.listen(3000, function () { 
-	console.log('App started at port 3000');
-});
+(async function () {
+	try {
+		await connectToDb();
+		app.listen(3000, function () { 
+			console.log('App started at port 3000');
+		});
+	} catch (err) {
+		console.log('ERROR: ', err);
+	}
+})();
