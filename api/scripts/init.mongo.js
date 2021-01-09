a8ffaf8765fa42128e690da97f70ad52cb6cbd97
Kosteyyy <averkiev@yandex.ru>
2021-01-09 17:04:59 +0300

    api & ui servers, eslint

--- api/scripts/init.mongo.js
+++ api/scripts/init.mongo.js
@@ -1,3 +1,6 @@
+
+// mongo issuetracker scripts/init.mongo.js
+
 /*
 * Run using the mongo shell. For remote databases, ensure that the
 * connection string is supplied in the command line. For example:
@@ -10,17 +13,33 @@ scripts/init.mongo.js
 * mongo mongodb://user:pwd@xxx.mlab.com:33533/issuetracker 
 scripts/init.mongo.js
 */
+
+
+/* global db print */
+/* eslint no-restricted-globals: "off" */
+
+
 db.issues.remove({});
 
 const issuesDB = [
-	{
-		id: 1, status: 'New', owner: 'Ravan', effort: 5, created: new Date('2018-08-15'), 
-		due: undefined, title: "New! Error in console when clicking Add",
-	},
-	{
-		id: 2, status: 'Assigned', owner: 'Eddie', effort: 14, created: new Date('2018-08-16'),
-		due: new Date('2018-08-30'), title: 'Missing bottom border on panel',
-	},
+  {
+    id: 1,
+    status: 'New',
+    owner: 'Ravan',
+    effort: 5,
+    created: new Date('2018-08-15'),
+    due: undefined,
+    title: 'New! Error in console when clicking Add',
+  },
+  {
+    id: 2,
+    status: 'Assigned',
+    owner: 'Eddie',
+    effort: 14,
+    created: new Date('2018-08-16'),
+    due: new Date('2018-08-30'),
+    title: 'Missing bottom border on panel',
+  },
 ];
 
 db.issues.insertMany(issuesDB);
